3483477fa267abc751d0bf4f887d73f5381f9c07
David Bogner <davidbogner@gmail.com>
2024-11-28 19:32:43 +0100

    UWK Fix simplecertificate PDF rendering

--- filter/tidy/filter.php
+++ filter/tidy/filter.php
@@ -50,6 +50,7 @@ class filter_tidy extends moodle_text_filter {
                  'drop-empty-paras' => true,
                  'indent' => true,
                  'quiet' => true,
+                 'wrap' => 0,
         );
 
     /// Do a quick check using strpos to avoid unnecessary work
